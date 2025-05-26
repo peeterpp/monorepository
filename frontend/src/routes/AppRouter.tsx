b5401b86c189087d1a1dca1ca040984154ffa9df
peeterpp <piotr.polinski9@wp.pl>
2025-05-26 17:07:26 +0200

    poprawki 1

--- frontend/src/routes/AppRouter.tsx
+++ frontend/src/routes/AppRouter.tsx
@@ -49,14 +49,14 @@ const App: React.FC = () => {
               </PrivateRoute>
             }
           />
-          <Route
+          {/* <Route
             path="/groups/:groupId/members"
             element={
               <PrivateRoute>
                 <GroupMembersPage />
               </PrivateRoute>
             }
-          />
+          /> */}
           <Route path="/groups/:groupId/debts" element={<GroupDebtsPage />} />
           <Route path="/test" element={<TestApiComponent />} />
           <Route
