8c0ab053ce89419a8d9165baa24f40c14dae474b
peeterpp <piotr.polinski9@wp.pl>
2025-05-26 17:19:54 +0200

    Poprawki3

--- frontend/src/routes/AppRouter.tsx
+++ frontend/src/routes/AppRouter.tsx
@@ -13,7 +13,6 @@ import HomePage from "../components/HomePage/HomePage";
 import PrivateRoute from "../components/Route/PrivateRoute";
 import PublicRoute from "../components/Route/PublicRoute";
 import BalanceBar from "../components/BalanceBar/BalanceBar";
-import GroupMembersPage from "../pages/GroupsPage/GroupMembersPage";
 import GroupDebtsPage from "../pages/GroupsPage/GroupDebtPage";
 import GroupsPage from "../pages/GroupsPage/GroupsPage";
 
@@ -49,14 +48,6 @@ const App: React.FC = () => {
               </PrivateRoute>
             }
           />
-          {/* <Route
-            path="/groups/:groupId/members"
-            element={
-              <PrivateRoute>
-                <GroupMembersPage />
-              </PrivateRoute>
-            }
-          /> */}
           <Route path="/groups/:groupId/debts" element={<GroupDebtsPage />} />
           <Route path="/test" element={<TestApiComponent />} />
           <Route
