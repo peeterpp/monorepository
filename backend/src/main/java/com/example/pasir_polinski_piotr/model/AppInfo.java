6bd65706de20d3746409fd15c3d53cec36d70ee4
peeterpp <piotr.polinski9@wp.pl>
2025-06-08 23:27:28 +0200

    Poprawki back 2

--- backend/src/main/java/com/example/pasir_polinski_piotr/model/AppInfo.java
+++ backend/src/main/java/com/example/pasir_polinski_piotr/model/AppInfo.java
@@ -9,7 +9,15 @@ import lombok.*;
 @Setter
 public class AppInfo {
     public String appName;
-    public String appVersion;
-    public String message;
+    private static final String appVersion = "1.0.0"; // Example version
+    private static final String message = "Welcome to the application!"; // Example message
+
+    public static String getAppVersion() {
+        return appVersion;
+    }
+
+    public static String getMessage() {
+        return message;
+    }
 }
 
